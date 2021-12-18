7e7e231577e1b6224f65aede4a051e6404f63391
Ignatii  Dubyshkin <kheldi@yandex.ru>
2021-12-18 17:12:13 +0300

    fix api

--- torchlsq/quantized/__init__.py
+++ torchlsq/quantized/__init__.py
@@ -3,34 +3,34 @@ from .modules.observers import LSQFakeQuantizer
 
 
 def disable_fake_quant(mod):
-    if isinstance(mod, torch.quantization.FakeQuantize) or isinstance(mod, LSQFakeQuantize):
+    if isinstance(mod, torch.quantization.FakeQuantize) or isinstance(mod, LSQFakeQuantizer):
         mod.disable_fake_quant()
 
 def enable_fake_quant(mod):
-    if isinstance(mod, torch.quantization.FakeQuantize) or isinstance(mod, LSQFakeQuantize):
+    if isinstance(mod, torch.quantization.FakeQuantize) or isinstance(mod, LSQFakeQuantizer):
         mod.enable_fake_quant()
 
 def disable_observer(mod):
-    if isinstance(mod, torch.quantization.FakeQuantize) or isinstance(mod, LSQFakeQuantize):
+    if isinstance(mod, torch.quantization.FakeQuantize) or isinstance(mod, LSQFakeQuantizer):
         mod.disable_observer()
 
 def enable_observer(mod):
-    if isinstance(mod, torch.quantization.FakeQuantize) or isinstance(mod, LSQFakeQuantize):
+    if isinstance(mod, torch.quantization.FakeQuantize) or isinstance(mod, LSQFakeQuantizer):
         mod.enable_observer()
 
 def disable_fake_quant_on_act(mod):
-    if isinstance(mod, torch.quantization.FakeQuantize) or isinstance(mod, LSQFakeQuantize) and mod.dtype==torch.quint8:
+    if isinstance(mod, torch.quantization.FakeQuantize) or isinstance(mod, LSQFakeQuantizer) and mod.dtype==torch.quint8:
         mod.disable_fake_quant()
 
 def enable_fake_quant_on_act(mod):
-    if isinstance(mod, torch.quantization.FakeQuantize) or isinstance(mod, LSQFakeQuantize) and mod.dtype==torch.quint8:
+    if isinstance(mod, torch.quantization.FakeQuantize) or isinstance(mod, LSQFakeQuantizer) and mod.dtype==torch.quint8:
         mod.enable_fake_quant()
 
 def disable_observer_on_weights(mod):
-    if isinstance(mod, torch.quantization.FakeQuantize) or isinstance(mod, LSQFakeQuantize) and mod.dtype==torch.qint8:
+    if isinstance(mod, torch.quantization.FakeQuantize) or isinstance(mod, LSQFakeQuantizer) and mod.dtype==torch.qint8:
         mod.disable_observer()
 
 def enable_observer_on_weights(mod):
-    if isinstance(mod, torch.quantization.FakeQuantize) or isinstance(mod, LSQFakeQuantize) and mod.dtype==torch.qint8:
+    if isinstance(mod, torch.quantization.FakeQuantize) or isinstance(mod, LSQFakeQuantizer) and mod.dtype==torch.qint8:
         mod.enable_observer()
 
